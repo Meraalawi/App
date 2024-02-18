804b9ee801aac905799b765a59d00fd03f02b156
Mera <Mira.alawi@harri.com>
2024-02-18 17:24:57 +0200

    Updated Automobile class

--- Automobile.java
+++ Automobile.java
@@ -1,55 +1,87 @@
-import java.sql.Date;
+import java.util.Date;
 
 public class Automobile {
-    private String Manufacture;
-    private Date ManufactureDate;
-    private Engine Engine;
-    private int PlateNumber;
-    private FuelType FuelType;
-    private GearType GearType;
-    private int SerialNumber;
+    private String manufacture;
+    private Date manufactureDate;
+    private Engine engine;
+    private int plateNumber;
+    private FuelType fuelType;
+    private GearType gearType;
+    private int serialNumber;
 
-    public String getManufacture(){
-        return Manufacture;
-    }
-    public void setManufacture(String Manufacture){
-        this.Manufacture=Manufacture;
-    }
-    public Date getManufactureDate(){
-        return ManufactureDate;
-    }
-    public void setManufactureDate(Date ManufactureDate){
-        this.ManufactureDate=ManufactureDate;
-    }
-    public Engine getEngine(){
-        return Engine;
-    }
-    public void setEngine(Engine Engine){
-        this.Engine=Engine;
-    }
-    public int getPlateNumber(){
-        return PlateNumber;
-    }
-    public void setPlateNumber(int PlateNumber){
-        this.PlateNumber=PlateNumber;
-    }
-    public FuelType getFuelType(){
-        return FuelType;
-    }
-    public void setFuelType(FuelType FuelType){
-        this.FuelType=FuelType;
-    }
-    public GearType getGearType(){
-        return GearType;
-    }
-    public void setGearType(GearType GearType){
-        this.GearType=GearType;
-    }
-    public int getSerialNumber(){
-        return SerialNumber;
-    }
-    public void SerialNumber(int SerialNumber){
-        this.SerialNumber=SerialNumber;
+    public Automobile() {
+        this.manufacture = null;
+        this.manufactureDate = new Date();
+        this.engine = null;
+        this.plateNumber = 0;
+        this.fuelType = null;
+        this.gearType = null;
+        this.serialNumber = 0;
     }
 
+    public Automobile(String manufacture, Date manufactureDate, Engine engine, int plateNumber, FuelType fuelType, GearType gearType, int serialNumber) {
+        this.manufacture = manufacture;
+        this.manufactureDate = manufactureDate;
+        this.engine = engine;
+        this.plateNumber = plateNumber;
+        this.fuelType = fuelType;
+        this.gearType = gearType;
+        this.serialNumber = serialNumber;
+    }
+
+    public String getManufacture() {
+        return manufacture;
+    }
+
+    public void setManufacture(String manufacture) {
+        this.manufacture = manufacture;
+    }
+
+    public Date getManufactureDate() {
+        return manufactureDate;
+    }
+
+    public void setManufactureDate(Date manufactureDate) {
+        this.manufactureDate = manufactureDate;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public int getPlateNumber() {
+        return plateNumber;
+    }
+
+    public void setPlateNumber(int plateNumber) {
+        this.plateNumber = plateNumber;
+    }
+
+    public FuelType getFuelType() {
+        return fuelType;
+    }
+
+    public void setFuelType(FuelType fuelType) {
+        this.fuelType = fuelType;
+    }
+
+    public GearType getGearType() {
+        return gearType;
+    }
+
+    public void setGearType(GearType gearType) {
+        this.gearType = gearType;
+    }
+
+    public int getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(int serialNumber) {
+        this.serialNumber = serialNumber;
+    }
 }
