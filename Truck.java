31723b7d754530381fc379a43b9c9c7829e01d36
Mera <Mira.alawi@harri.com>
2024-07-01 10:43:44 +0300

    add print()

--- Truck.java
+++ Truck.java
@@ -29,4 +29,20 @@ public class Truck extends Vehicle {
     public void setFullWeight(double fullWeight) {
         this.fullWeight = fullWeight;
     }
+    public void print() {
+        System.out.println("Truck Details:");
+        System.out.println("Width: " + getWidth());
+        System.out.println("Length: " + getLength());
+        System.out.println("Color: " + getColor());
+        System.out.println("Free Weight: " + freeWeight);
+        System.out.println("Full Weight: " + fullWeight);
+        System.out.println("Plate Number: " + getPlateNumber());
+        System.out.println("Serial Number: " + getSerialNumber());
+        System.out.println("Manufacturer: " + getManufacture());
+        System.out.println("Manufacture Date: " + getManufactureDate());
+        System.out.println("Fuel Type: " + getFuelType());
+        System.out.println("Gear Type: " + getGearType());
+        System.out.println("Engine: ");
+        getEngine().print();
+    }
 }
